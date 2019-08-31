0022fb336fa7a9d8f17040000d0625e148339cee
nitinpatil1992 <patil1611nit@gmail.com>
2019-08-31 13:33:36 +0200

    added env variable for api host

--- verso/main.go
+++ verso/main.go
@@ -1,12 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"os"
 )
 
+var (
+	apiHost string = getenv("API_HOST", "127.0.0.1:8000")
+)
+
+func getenv(key, fallback string) string {
+	value := os.Getenv(key)
+	if len(value) == 0 {
+		return fallback
+	}
+	return value
+}
+
 func main() {
-	apiHost := os.Getenv("API_HOST")
+	//apiHost := os.Getenv("API_HOST")
 	http.HandleFunc("/reverse", HandleReverse)
+	fmt.Println("starting server on", apiHost)
 	http.ListenAndServe(apiHost, nil)
 }
