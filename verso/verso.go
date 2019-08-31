74c9a26d2dd9a4d2937bc0ed58082e603ebbd9ad
nitinpatil1992 <patil1611nit@gmail.com>
2019-08-31 13:16:55 +0200

    Added reverse string handler

--- verso/verso.go
+++ verso/verso.go
@@ -1,5 +1,34 @@
 package main
 
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type Request struct {
+	Message string
+}
+
+func HandleReverse(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+	}
+	decoder := json.NewDecoder(r.Body)
+
+	var request Request
+	response := make(map[string]interface{})
+
+	err := decoder.Decode(&request)
+	if err != nil {
+		panic("Invalid request data")
+	}
+
+	response["message"] = reverse(request.Message)
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(response)
+}
+
 func reverse(input string) string {
 	output := []byte(input)
 	l, r := 0, len(input)-1
